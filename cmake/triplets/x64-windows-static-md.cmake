4eb4ddf84f585fc28ee7db93a061a56130b329d3
SulfurNitride <182011199+SulfurNitride@users.noreply.github.com>
2026-07-30 19:17:01 -0500

    Pass pkg-config through vcpkg

--- cmake/triplets/x64-windows-static-md.cmake
+++ cmake/triplets/x64-windows-static-md.cmake
@@ -2,3 +2,8 @@ set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_BUILD_TYPE release)
+
+# Keep the current pkg-config supplied by CI visible while vcpkg builds ports.
+set(VCPKG_ENV_PASSTHROUGH_UNTRACKED
+    PKG_CONFIG
+)
